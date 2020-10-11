3026c7158f0c0c08ce9908a2d073192c9cf13aff
ysabel <ysabel.titan@gmail.com>
2020-10-11 15:07:16 -0700

    fixed stuck at right boundary issue

--- src/Ball.cpp
+++ src/Ball.cpp
@@ -15,7 +15,6 @@ const int VELOCITY_INCREMENT = 2;
 Ball::Ball() {}
 Ball::~Ball() {}
 
-//void Ball::init(int x, int y) {
 void Ball::init() {
     // loads file as texture
     ballTex = TextureManager::LoadTexture(BALL_FILE);
@@ -39,43 +38,6 @@ void Ball::init() {
 
 }
 
-/*
-// returns false if the ball hits the bottom of the screen else returns true
-bool Ball::move(Player* player1) {
-
-    if(dest.y + dest.h >= SCREEN_HEIGHT) {
-        return false; // GAME OVER
-    }
-
-    //hits player paddle
-    if (SDL_HasIntersection(player1->getRect(), &dest)) {
-        velocity_y *= -1;
-        velocity_x = rand() % ANGLE_RANGE - (ANGLE_RANGE-1) / 2;
-    }
-
-    // TODO increment velocity after clearing a level (ie clearing all blocks from a map)
-    //right boundary, dest.x is for upper left corner
-    if (dest.x + dest.w >= SCREEN_WIDTH) {
-        dest.x = SCREEN_WIDTH - dest.w; 
-        velocity_x *= -1;
-    }
-
-    // left boundary, dest.y is for upper left corner of the ball
-    if (dest.x <= 0) {
-        dest.x = 0; 
-        velocity_x *= -1;
-    }
-
-    if (dest.y <= 0) {
-    	dest.y = 0;
-        velocity_y *= -1;
-    }
-
-    dest.x += velocity_x;
-    dest.y += velocity_y;
-    return true;
-}
-*/
 void Ball::render() {
     TextureManager::Draw(ballTex, &dest);
 }
